fd803d6490572d72f762a749d0b3b4fa6bcf736b
Cécile Hirschauer <59025247+Cecile-Hirschauer@users.noreply.github.com>
2024-11-21 15:46:04 +0100

    Delete frontend directory

--- frontend/app/components/utils/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { Spinner, Flex } from "@chakra-ui/react";
-
-const Loader = ({ size, mt }) => (
-  <Flex justifyContent="center">
-    <Spinner {...{ size, mt }} />
-  </Flex>
-);
-
-export default Loader;
